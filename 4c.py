0b2c33d6b5a5a2300eb512e19d568ec2942259e2
Rahul Gupta <42673573+rahulzxcvbn@users.noreply.github.com>
2020-11-30 22:14:26 +0530

    Create 4c.py

---
+++ 4c.py
@@ -0,0 +1,23 @@
+"""
+Youtube Video Link :- https://youtu.be/fiicYR__4Us
+
+Write a Python program to clone or copy a list
+
+Save File as 4c.py
+Run :- python 4c.py
+
+"""
+#Method1
+original_list=[1,2,3,4,5]
+print("Original List = ",original_list)
+new_list=list(original_list)
+print("New List = ",new_list)
+
+print()
+
+#Method
+og=[6,7,8,9,10]
+print("OG = ",og)
+copy=og.copy()
+print("COPY = ",copy)
+print("Rajan Gupta")
