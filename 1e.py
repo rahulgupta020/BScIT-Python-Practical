b816eeb625305fe2a53272302a76b4d386f73600
Rahul Gupta <42673573+rahulzxcvbn@users.noreply.github.com>
2020-11-30 20:23:30 +0530

    Update 1e.py
    Write a function to check the input value is Armstrong and also write the
    function for Palindrome.

--- 1e.py
+++ 1e.py
@@ -1,1 +1,36 @@
+"""
+Youtube Video Link :- https://youtu.be/_9KCrG9Sasg
 
+Save File as 1e.py
+Run :- python 1e.py
+
+"""
+def Armstrong(n):
+  temp=n
+  result=0
+  while(temp!=0):
+    remainder=temp%10
+    result=remainder**3+result
+    temp=temp//10
+
+  if(result==n):
+    print(n," is armstrong number ")
+  else:
+    print(n, "not an armstrong number ")
+
+def Palindrome(n):
+  temp=n
+  reverse=0
+  while(temp!=0):
+    remainder=temp%10
+    reverse=reverse*10+remainder
+    temp=temp//10
+
+  if(n==reverse):
+    print(n," is Palindrome")
+  else:
+    print(n," is not Palindrome")
+
+n=int(input("Enter a number :- "))
+Armstrong(n)
+Palindrome(n)
