16a782e78154cfae347da738d519f1646f35a042
Rahul Gupta <42673573+rahulzxcvbn@users.noreply.github.com>
2020-11-30 22:24:22 +0530

    Create 6a.py

---
+++ 6a.py
@@ -0,0 +1,12 @@
+"""
+Youtube Video Link :- https://youtu.be/6aNV2S_QqOU
+
+Write a Python program to read an entire text file
+
+Save File as 6a.py
+Run :- python 6a.py
+
+"""
+f = open("myfile.txt","r")
+print(f.read())
+f.close()
