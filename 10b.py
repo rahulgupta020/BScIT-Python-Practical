bcd588e399b2e70e31711ba909f6d5609b76068f
Rahul Gupta <42673573+rahulzxcvbn@users.noreply.github.com>
2020-11-30 22:58:29 +0530

    Create 10b.py

---
+++ 10b.py
@@ -0,0 +1,25 @@
+"""
+Youtube Video Link :- https://youtu.be/PedX5TQZuPQ
+
+Design a database application to search the specified record from the database
+
+Save File as 10b.py
+Run :- python 10b.py
+
+"""
+import mysql.connector
+
+mydb = mysql.connector.connect(
+	host="localhost",
+	username="root",
+	password="",
+	database="bscit"
+	)
+# print(mydb)
+mycursor = mydb.cursor()
+
+mycursor.execute("select * from students where age > '22'")
+result = mycursor.fetchall()
+#print(result)
+for x in result:
+	print(x)
