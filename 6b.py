c1d648a559e002f37bfb3ab4ffc2464da5a7d6e1
Rahul Gupta <42673573+rahulzxcvbn@users.noreply.github.com>
2020-11-30 22:25:59 +0530

    Create 6b.py

---
+++ 6b.py
@@ -0,0 +1,12 @@
+"""
+Youtube Video Link :- https://youtu.be/V6as2WNAiaE
+
+Write a Python program to append text to a file and display the text.
+
+Save File as 6b.py
+Run :- python 6b.py
+
+"""
+f = open("myfile.txt","a")
+f.write("\nhello world")
+f.close()
